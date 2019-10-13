5e5b1dfd911a0d50483e2feaa9261a79afaa1beb
n.tselishchev <n.tselishchev@gmail.com>
2019-10-13 14:21:28 +0300

    lesson-31

--- src/main/java/com/ntselishchev/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/ntselishchev/libraryapp/LibraryAppApplication.java
@@ -2,10 +2,12 @@ package com.ntselishchev.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
 @IntegrationComponentScan
+@EnableCircuitBreaker
 public class LibraryAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ntselishchev/libraryapp/service/LibraryServiceImpl.java
+++ src/main/java/com/ntselishchev/libraryapp/service/LibraryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ntselishchev.libraryapp.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.ntselishchev.libraryapp.dao.AuthorDao;
 import com.ntselishchev.libraryapp.dao.BookDao;
 import com.ntselishchev.libraryapp.dao.GenreDao;
@@ -9,10 +10,13 @@ import com.ntselishchev.libraryapp.domain.Genre;
 import com.ntselishchev.libraryapp.dto.BookDTO;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,11 +24,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LibraryServiceImpl implements LibraryService {
 
+    private static final Logger availabilityLog = LoggerFactory.getLogger("availability-data");
+
     private final BookDao bookDao;
     private final AuthorDao authorDao;
     private final GenreDao genreDao;
     private final MeterRegistry metric;
 
+    @HystrixCommand(commandKey = "addBookKey", fallbackMethod = "buildAddBookFallback")
     public void addBook(BookDTO bookDto) {
         Optional<Author> author = authorDao.findById(bookDto.getAuthorId());
         Optional<Genre> genre = genreDao.findById(bookDto.getGenreId());
@@ -37,11 +44,13 @@ public class LibraryServiceImpl implements LibraryService {
         }));
     }
 
+    @HystrixCommand(commandKey = "deleteBookKey", fallbackMethod = "buildDeleteBookFallback")
     public void deleteBook(String id) {
         bookDao.deleteById(id);
         metric.counter("books.deleted").increment();
     }
 
+    @HystrixCommand(commandKey = "updateBookKey", fallbackMethod = "buildUpdateBookFallback")
     public void updateBook(BookDTO bookDto) {
         Optional<Book> book = bookDao.findById(bookDto.getId());
         Optional<Author> author = authorDao.findById(bookDto.getAuthorId());
@@ -60,17 +69,47 @@ public class LibraryServiceImpl implements LibraryService {
         );
     }
 
+    @HystrixCommand(commandKey = "getAuthorsKey", fallbackMethod = "buildGetAuthorsFallback")
     public List<Author> getAuthors() {
         return authorDao.findAll();
     }
 
+    @HystrixCommand(commandKey = "getGenresKey", fallbackMethod = "buildGetGenresFallback")
     public List<Genre> getGenres() {
         return genreDao.findAll();
     }
 
     @PreAuthorize("hasAuthority('USER')")
     @PostFilter("hasPermission(filterObject, 'READ')")
+    @HystrixCommand(commandKey = "getBooksKey", fallbackMethod = "buildGetBooksFallback")
     public List<Book> getBooks() {
         return bookDao.findAll();
+    }
+
+    public List<Book> buildGetBooksFallback() {
+        availabilityLog.error("unable to invoke DB in getBooks");
+        return Collections.emptyList();
+    }
+
+    public List<Author> buildGetAuthorsFallback() {
+        availabilityLog.error("unable to invoke DB in getAuthors");
+        return Collections.emptyList();
+    }
+
+    public List<Genre> buildGetGenresFallback() {
+        availabilityLog.error("unable to invoke DB in getGenres");
+        return Collections.emptyList();
+    }
+
+    public void addBookFallback() {
+        availabilityLog.error("unable to invoke DB in addBook");
+    }
+
+    public void deleteBookFallback() {
+        availabilityLog.error("unable to invoke DB in deleteBook");
+    }
+
+    public void updateBookFallback() {
+        availabilityLog.error("unable to invoke DB in updateBook");
     }
 }
